82d4fa18cf47bacf0d82b6012e789b6bd2a7d1b6
zlj <xiaotseng@gmail.com>
2018-08-28 22:31:14 +0800

    tubiao

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-from flask import make_response
-from flask import request
-from flask import render_template
+from flask import Flask, make_response, render_template, request
 from flask_bootstrap import Bootstrap
+
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
@@ -10,7 +8,7 @@ bootstrap = Bootstrap(app)
 @app.route("/")
 def index():
     return render_template("index.html")
-
+    
 @app.route("/user/<name>")
 def user(name):
     return render_template("user.html", name=name)
