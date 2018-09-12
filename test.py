3e5051ed3c6f7c843b86f88d6867e1c231dc59ba
lingel <lingel2000@msn.com>
2018-09-12 13:20:54 +0800

    Null

--- test.py
+++ test.py
@@ -1,25 +1,11 @@
-import time
-
-def decorator(func):
-    def wrapper(*args, **kwargs):
-        print("列表参数",end=" ")
-        print(args)
-        print("字典参数",end=" ")
-        print(kwargs)
-        start_time = time.time()
-        func(*args, **kwargs)
-        end_time = time.time()
-        print(end_time - start_time)
-    return wrapper
-
-@decorator 
-def func():
-    time.sleep(0.8)
-
-def sum(a,b,c):
-    print(a+b+c)
-
-func2=decorator(sum)
-
-func2(1,2,3) # 函数调用
-# 输出：0.800644397735595
+from flask_script import Shell,Manager
+from threading import Thread
+from mods import User,Role,db
+db.make_connector()
+from appCreator import app
+manager=Manager(app)
+def mak_shell_context():
+    return dict(app=app,db=db,User=User,Role=Role)
+manager.add_command("shell",Shell(make_context=mak_shell_context))
+if __name__=="__main__":
+    manager.run()
